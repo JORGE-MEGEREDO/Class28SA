e50fc62b8dfffa5b7e92c6120d8a5b2ed5121cba
JORGE-MEGEREDO <114191444+JORGE-MEGEREDO@users.noreply.github.com>
2023-01-31 18:27:54 -0300

    Update Cannon.js

--- js/Cannon.js
+++ js/Cannon.js
@@ -7,7 +7,13 @@ class Cannon {
     this.angle = angle;
   }
   display() {
-   
+    if (keyIsDown(RIGHT_ARROW) && this.angle < 0.35) {
+      this.angle += 0.02;
+      }
+      if (keyIsDown(LEFT ARROW) && this.angle> -1.45) {
+        this.angle -= 0.02;
+      }   
+        
     fill("#676e6a");
     push();
     translate(this.x, this.y);
